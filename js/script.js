33dcfb410532432b92dea926b290b218f984482a
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-06 18:40:36 +0200

    [UPD] fixed script.js; updated style.css

--- js/script.js
+++ js/script.js
@@ -4,27 +4,28 @@ function createSquare() {
     
     // asegnazione della classe square all'elemento creato
     currentElement.classList.add('square')
+    console.log(currentElement);
 
-    // recupero l'elemento che conterrà la mia griglia
-    const grid = document.getElementById('grid')
+    //restituisco il quadrato contenuto nella variabile currentElement
+    return currentElement;
 }
 
+// recupero l'elemento che conterrà la mia griglia
+const grid = document.getElementById('grid')
 
 // eseguo un ciclo di 100 iterazioni per creare una griglia 10x10
 for (let i = 0; i < 100; i++) {
     //creo il sngolo quadrato chiamando la funzione createSquare
     let currentSquare = createSquare();
 
-    currentSquare.addEventLitener('click', function () {
-        console.log(this)
-        this.classList.toggle('clicked')
-    })
+    //aggiungo l'evento click al qyadrato creato 
+    currentSquare.addEventListener('click', function () {
+        console.log(this);
+        this.classList.toggle('clicked');
+    });
 
     // gli metto il numero progressivo
     currentSquare.innerText = i + 1;
-    
-    //assegnazione della classe square all'elemento creato
-    currentSquare.classList.add('square')
 
     // appendo il quadrato appena creato all'interno della griglia presa precedentemente
     grid.append(currentSquare);
